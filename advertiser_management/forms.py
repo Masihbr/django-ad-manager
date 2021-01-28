9d695abcdff3c975ab1c6ceebf2b23f966f1d0f9
Masihbr <masihbr@gmail.com>
2021-01-28 20:32:48 +0330

    fixed form issues

--- advertiser_management/forms.py
+++ advertiser_management/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class AdForm(forms.Form):
-    advertiser_id = forms.IntegerField(label='advertiser_id')
-    image = forms.ImageField(label='image')
-    title = forms.CharField(label='advertiser_id', max_length=100)
-    url = forms.URLField(label='url')
+    advertiser_id = forms.IntegerField(label='Advertiser_id')
+    image = forms.ImageField(label='Image')
+    title = forms.CharField(label='Title', max_length=100)
+    url = forms.URLField(label='Url')
