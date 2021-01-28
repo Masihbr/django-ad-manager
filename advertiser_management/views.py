d7ec5199334397ef81192ffd43629c26b870bebe
Masihbr <masihbr@gmail.com>
2021-01-28 18:05:24 +0330

    added advertiser_management to apps in setting

--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'advertiser_management/ads.html', context={})
