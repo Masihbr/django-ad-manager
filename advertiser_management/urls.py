065b318c713c68006dd2a4a77f8b2e3daa996fc6
Masihbr <masihbr@gmail.com>
2021-02-01 21:52:30 +0330

    made home view function a TemplateView class

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import Clicker,CreateAd
+from .views import Clicker, CreateAd, HomePageView
 
 urlpatterns = [
-    path('', views.home, name='ads'),
+    path('', HomePageView.as_view(), name='ads'),
     path('click/<int:pk>/', Clicker.as_view(), name='clicker'),
     path('ad/<int:pk>/', views.ad, name='ad'),
     path('create_ad/', CreateAd.as_view(), name='create_ad')
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -4,15 +4,16 @@ from django.views.generic import CreateView
 from .forms import create_ad_form
 from .models import Advertiser, Ad, Click, View
 from django.shortcuts import get_object_or_404
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 
 
-# Create your views here.
-def home(request):
-    context = {
-        'advertisers': Advertiser.objects.all()
-    }
-    return render(request, 'advertiser_management/ads.html', context)
+class HomePageView(TemplateView):
+    template_name = 'advertiser_management/ads.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['advertisers'] = Advertiser.objects.all()
+        return context
 
 
 class Clicker(RedirectView):
