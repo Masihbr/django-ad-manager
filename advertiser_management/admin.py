951f1a92ad69dbcca50103afb50e1880ae419b19
Masihbr <masihbr@gmail.com>
2021-02-01 21:32:57 +0330

    filter and search for ad

--- advertiser_management/admin.py
+++ advertiser_management/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Ad,Advertiser,Click,View
 # Register your models here.
 
-admin.site.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title','advertiser','url','approve')
+    search_fields = ('title',)
+    filter_horizontal = ('approve',)
+
+admin.site.register(Ad, AdAdmin)
 admin.site.register(Advertiser)
 admin.site.register(Click)
 admin.site.register(View)
