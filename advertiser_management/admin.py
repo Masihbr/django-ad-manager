5c6de0531a83dc3cacdf4cd76497ec6c6bc2ca6b
Masihbr <masihbr@gmail.com>
2021-02-01 21:39:20 +0330

    fixe filter

--- advertiser_management/admin.py
+++ advertiser_management/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Ad,Advertiser,Click,View
+from .models import Ad, Advertiser, Click, View
+
+
 # Register your models here.
 
 class AdAdmin(admin.ModelAdmin):
-    list_display = ('title','advertiser','url','approve')
+    list_display = ('title', 'advertiser', 'url', 'approve')
     search_fields = ('title',)
-    filter_horizontal = ('approve',)
+    list_filter = ('approve',)
+
 
 admin.site.register(Ad, AdAdmin)
 admin.site.register(Advertiser)
