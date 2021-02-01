b6d673ccf7f3d0d29cfad5440e5655992303f01b
Masihbr <masihbr@gmail.com>
2021-02-01 23:45:02 +0330

    added report url

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import Clicker, CreateAdPage, HomePageView, AdPageView
+from .views import Clicker, CreateAdPage, HomePageView, AdPageView, ReportPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='ads'),
     path('click/<int:pk>/', Clicker.as_view(), name='clicker'),
     path('ad/<int:pk>/', AdPageView.as_view(), name='ad'),
-    path('create_ad/', CreateAdPage.as_view(), name='create_ad')
+    path('create_ad/', CreateAdPage.as_view(), name='create_ad'),
+    path('report/', ReportPageView.as_view(), name='report')
 ]
