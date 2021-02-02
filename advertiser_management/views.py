ded381c7093df3a5ae45316ba70050042ac71bd2
Masihbr <masihbr@gmail.com>
2021-02-02 14:56:32 +0330

    clicks per views ratio done

--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -52,16 +52,38 @@ class ReportPageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        clicks_views_sum_list = self.clicks_views_sum_per_hour_each_ad()
+        clicks_views_sum_list = self.get_clicks_views_sum_list()
+        clicks_per_views_list = self.get_clicks_per_view_list()
 
         context = {
+            'clicks_per_views_list': clicks_per_views_list,
             'clicks_views_sum_list': clicks_views_sum_list,
             'ads_list': Ad.objects.all()
         }
         return context
 
-    def clicks_views_sum_per_hour_each_ad(self):
-        big_list = {}
+    def get_clicks_per_view_list(self):
+        clicks_per_views_list = {}
+        for ad in Ad.objects.all():
+            list = {}
+            clicks_per_hour = Click.objects.annotate(
+                hour=TruncHour('time', output_field=DateTimeField()), ).values('hour').filter(ad=ad).annotate(
+                clicks=Count('id'))
+            views_per_hour = View.objects.annotate(
+                hour=TruncHour('time', output_field=DateTimeField()), ).values('hour').filter(ad=ad).annotate(
+                views=Count('id'))
+            for v in views_per_hour:
+                list[v['hour']] = v['views']
+            for c in clicks_per_hour:
+                if list.keys().__contains__(c['hour']):
+                    list[c['hour']] = c['clicks'] / list[c['hour']]
+                else:
+                    list[c['hour']] = 0
+            clicks_per_views_list[ad] = sorted(list.items(),reverse=True, key=lambda t: t[1])
+        return clicks_per_views_list
+
+    def get_clicks_views_sum_list(self):
+        clicks_views_sum_list = {}
         for ad in Ad.objects.all():
             list = {}
             clicks_per_hour = Click.objects.annotate(
@@ -77,5 +99,5 @@ class ReportPageView(TemplateView):
                     list[v['hour']] += v['views']
                 else:
                     list[v['hour']] = v['views']
-            big_list[ad] = sorted(list.items(), key=lambda t: t[0])
-        return big_list
+            clicks_views_sum_list[ad] = sorted(list.items(), key=lambda t: t[0])
+        return clicks_views_sum_list
