63c34903de04b8a565ba8369c01fbcfa43ae05fc
Masihbr <masihbr@gmail.com>
2021-02-04 10:30:06 +0330

    using ModelSerializer

--- advertiser_management/serializers.py
+++ advertiser_management/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from advertiser_management.models import Ad, Advertiser, Click, View
 
 
-class AdSerializer(serializers.Serializer):
+class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         fields = '__all__'
@@ -12,7 +12,7 @@ class AdSerializer(serializers.Serializer):
         return Ad.objects.create(**validated_data)
 
 
-class AdvertiserSerializer(serializers.Serializer):
+class AdvertiserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advertiser
         fields = '__all__'
@@ -21,13 +21,13 @@ class AdvertiserSerializer(serializers.Serializer):
         return Advertiser.objects.create(**validated_data)
 
 
-class ClickSerializer(serializers.Serializer):
+class ClickSerializer(serializers.ModelSerializer):
     class Meta:
         model = Click
         fields = '__all__'
 
 
-class ViewSerializer(serializers.Serializer):
+class ViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = View
         fields = '__all__'
