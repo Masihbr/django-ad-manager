31210506bec6b5119a75db7b83325b72d675d36f
Masihbr <masihbr@gmail.com>
2021-02-05 20:56:00 +0330

    added HourTable Task method

--- advertiser_management/tasks.py
+++ advertiser_management/tasks.py
@@ -1,14 +1,21 @@
-from celery import shared_task
+from datetime import timezone, timedelta
 
+from celery import shared_task
+from .models import Ad, Click, View, HourStatusTable
 import time
 
 
 @shared_task
-def sum(a, b):
-    time.sleep(10)
-    return a + b
+def save_hour_ad_status():
+    ads = Ad.objects.all()
+    this_hour = timezone.now().replace(minute=0, second=0, microsecond=0)
+    past_hour = this_hour - timedelta(hours=1)
+    for ad in ads:
+        clicks = Click.objects.filter(ad=ad, date__range=(past_hour, this_hour)).count()
+        views = View.objects.filter(ad=ad, date__range=(past_hour, this_hour)).count()
+        table = HourStatusTable(ad=ad, clicks=clicks, views=views, date=this_hour)
+        table.save()
 
 
-@shared_task
-def send_email(email):
-    print(f'A sample message is sent to {email}')
+# @shared_task
+
