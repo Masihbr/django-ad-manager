2a983cfa5b2af2e3279d55c50c49c79588fe8381
Masihbr <masihbr@gmail.com>
2021-02-05 10:50:31 +0330

    deleted unneccessary functions

--- advertiser_management/serializers.py
+++ advertiser_management/serializers.py
@@ -8,26 +8,11 @@ class AdSerializer(serializers.ModelSerializer):
         model = Ad
         fields = '__all__'
 
-    def create(self, validated_data):
-        return Ad.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.image = validated_data.get('image', instance.image)
-        instance.advertiser = validated_data.get('advertiser', instance.advertiser)
-        instance.url = validated_data.get('url', instance.url)
-        instance.approve = validated_data.get('approve', instance.approve)
-        instance.save()
-        return instance
-
 
 class AdvertiserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advertiser
         fields = '__all__'
-
-    def create(self, validated_data):
-        return Advertiser.objects.create(**validated_data)
 
 
 class ClickSerializer(serializers.ModelSerializer):
