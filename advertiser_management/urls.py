a49eb3515268cff085e8acd9d0eee0dfaf30b9ca
Masihbr <masihbr@gmail.com>
2021-02-06 11:24:32 +0330

    rest for report

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (Clicker, CreateAdPage, HomePageView, AdPageView, ReportPageView, AdViewset, AdvertiserViewset,
-                    ClickViewset, ViewViewset)
+                    ClickViewset, ViewViewset, ViewReportPageAPIView)
 
 router = DefaultRouter()
 router.register(r'ad_list', AdViewset, basename='ad_list')
 router.register(r'advertiser_list', AdvertiserViewset, basename='advertiser_list')
 router.register(r'click_list', ClickViewset, basename='click_list')
 router.register(r'view_list', ViewViewset, basename='view_list')
+router.register(r'view_report_list', ViewReportPageAPIView, basename='view_report_list')
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='ads'),
